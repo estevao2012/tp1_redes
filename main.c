27ccd580de9207fc17fcc92e08c1089d970297a9
Estevão Moraes <estevao@uololou.com.br>
2013-08-31 13:16:37 -0300

    Primeira conexao realizada com sucesso ao servidor[v:estavel]

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+ 
+#define MAXRCVLEN 500
+#define PORTNUM 80
+ 
+int main(int argc, char *argv[])
+{
+   char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
+   char msg[] = "Atestecone1 e5e9938b0c";
+   int len, mysocket;
+   struct sockaddr_in dest; 
+ 
+   mysocket = socket(AF_INET, SOCK_STREAM, 0);
+ 
+   memset(&dest, 0, sizeof(dest));                /* zero the struct */
+   dest.sin_family = AF_INET; 
+   // dest.sin_addr.s_addr = gethostbyname("george.dcc.ufmg.br");///; /* set destination IP number */ 
+   dest.sin_addr.s_addr = inet_addr("150.164.9.28");///; /* set destination IP number */ 
+   dest.sin_port = htons(PORTNUM);                /* set destination port number */
+
+   connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+    
+   send(mysocket, msg, strlen(msg), 0);  
+ 
+   len = recv(mysocket, buffer, MAXRCVLEN, 0);
+   buffer[len] = '\0'; 
+   printf("Received %s (%d bytes).\n", buffer, len);
+ 
+   close(mysocket);
+   return EXIT_SUCCESS;
+}
