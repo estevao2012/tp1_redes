4e1b24a639e7892efc307ba5c2c8168e3cf1bf80
Usuario <dcc@localhost.localdomain>
2013-09-03 20:17:20 -0300

    desenvolver fazer conversao hexadecimal

--- main.c
+++ main.c
@@ -16,37 +16,94 @@
 
 int main(int argc, char *argv[])
 {
-   char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-   char msg[] = "Atatrdconew e5e9938b0c";
-   int len, mysocket;
-   struct sockaddr_in dest; 
-   char *hostname = "george.dcc.ufmg.br";
-   char ip[100];
-   char *error;
+    char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
+    char msg[] = "Atestecone2 8a114d6895";
+    int numbytes, mysocket, i;
+    struct sockaddr_in dest;
+    char *hostname = "george.dcc.ufmg.br";
+    char ip[100];
+    char *error;
+    
+    hostname_to_ip(hostname , ip);
+    
+    if ((mysocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket()");
+        exit(1);
+    }
+    else
+    {
+        printf("the socket is ok\n");
+    }
+    
+    memset(&dest, 0, sizeof(dest));           /* zero the struct */
+    dest.sin_family = AF_INET;
+    dest.sin_addr.s_addr = inet_addr(ip);     /* set destination IP number */
+    // dest.sin_addr.s_addr = inet_addr("127.0.0.1");
+    dest.sin_port = htons(PORTNUM);           /* set destination port number */
+    
+    if (connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == -1)
+    {
+        perror("connect()");
+        exit(1);
+    }
+    else
+    {
+        printf("The connect() is OK\n");
+    }
+    
+    if (send(mysocket, msg, strlen(msg), 0) == -1)//envia conexao 'A' ja com senha certa
+    {
+        perror("send");
+        exit(1);
+    }
+    else
+    { printf("msg send\n"); }
+    
+    if ((numbytes = recv(mysocket, buffer, MAXRCVLEN, 0)) == -1)//Recebe resposta
+    {
+        perror("recv()");
+        exit(1);
+    }
+    
+    buffer[numbytes] = '\0';
 
-   hostname_to_ip(hostname , ip); 
-   
-   mysocket = socket(AF_INET, SOCK_STREAM, 0);
+    if ( buffer[0] == 'C')
+    {
  
-   memset(&dest, 0, sizeof(dest));                /* zero the struct */
-   dest.sin_family = AF_INET; 
-   dest.sin_addr.s_addr = inet_addr(ip);///; /* set destination IP number */ 
-   // dest.sin_addr.s_addr = inet_addr("127.0.0.1");
-   dest.sin_port = htons(PORTNUM);                /* set destination port number */
+        printf("Conexão via endereço IP:%i.%i.%i.%i\n", (unsigned int)buffer[1],(unsigned int)buffer[2],(unsigned int)buffer[3],(unsigned int)buffer[4]);
+    }
+    else if (buffer[0] == 'N')
+    {
+	char hostname_recv[100];
+	int port_recv;
 
-   if (connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == -1) {
-      // perror(error);
-      // printf("%s\n",error);
-      printf ("-- Error when stablishing a connection\n");
-      return -1;
-   } else{ 
-         send(mysocket, msg, strlen(msg), 0) ; 
-         len = recv(mysocket, buffer, MAXRCVLEN, 0);
-         buffer[len] = '\0';    
-         printf("Received %s (%d bytes).\n", buffer,len);
-         buffer[0] = 82;
-         printf("%s %d", buffer,len); 
-   } 
-   close(mysocket);
-   return EXIT_SUCCESS;
+        for (i=2; i<buffer[1]+2; i++)
+	{
+		hostname_recv[i-2] = buffer[i];
+	}
+	port_recv = buffer[i]*256+buffer[i+1];
+        printf("Conexão via nome do servidor:%s | Porta: %i", hostname_recv, port_recv);        
+	printf("\n");        
+    }
+    else if (buffer[0] == 'S')
+    {
+        printf("Saida");
+    }
+    else if (buffer[0] == 'D')
+    {
+        printf("Resultado");
+    }
+    else if (buffer[0] == '0')
+    {
+        printf("OK");
+    }
+    else if (buffer[0] == 'E')
+    {
+        printf("ERRO");
+    }
+    printf("(%d bytes).\n", buffer[0],numbytes);
+    
+    close(mysocket);
+    return EXIT_SUCCESS;
 }
