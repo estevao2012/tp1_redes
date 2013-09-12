2002e61ba85f0e400b707b9549f6eaac664d9d75
Estevão Moraes <estevao@uololou.com.br>
2013-09-12 00:35:09 -0300

    Problema com resposta hexadecimal ao servidor

--- main.c
+++ main.c
@@ -19,10 +19,12 @@ int main(int argc, char *argv[])
     char resposta[MAXRCVLEN + 1]; 
     char msg[] = "Atestecone2 8a114d6895";
     char *hostname = "george.dcc.ufmg.br";
-    char ip[100]; 
-    char ip_recv[100];  
-    int  i,j=0; 
-    unsigned short port_recv;     
+    char ip[16]; 
+    char ip_recv[16]; 
+    char num[3]; 
+    int  i,j=0,h=0,tmp_ip; 
+    unsigned short port_recv; 
+
     //---------  Variaveis --------// 
 
     int numbytes, mysocket;
@@ -95,12 +97,62 @@ int main(int argc, char *argv[])
 
         }else if (resposta[0] == 'S'){	
 
-            printf("Saida: O cliente deve fechar a conexão e informar ao servidor a ultima conexao realizada");
-            printf("A mensagem de resultado comeca com o caracter D seguido de endereco ip e porta em hexadecimal\n");       
+            // printf("Saida: O cliente deve fechar a conexão e informar ao servidor a ultima conexao realizada");
+            // printf("A mensagem de resultado comeca com o caracter D seguido de endereco ip e porta em hexadecimal\n");       
+            // memset(msg ,"",sizeof(msg));
+            // int addr = inet_addr(ip);
+            //Convert Ip to hexaDecimal
+            char final[] = "D"; 
+            int total_caracter=0;
+            int pos_hex=1;
+            for(i=0;i<sizeof(ip);i++)
+                if(ip[i] != NULL)
+                    total_caracter++;
+
+            for(i=0;i<total_caracter;i++){ 
+                    if(ip[i] != '.' && ip[i] != NULL){ 
+                        num[h] = ip[i];
+                        h++;
+                    }else{
+                        h = 0;
+                        tmp_ip = atoi(num);
+                        num[2] = NULL;
+                        num[1] = NULL;
+                        num[0] = NULL;
+                        printf("\\x%X\n", ntohs(tmp_ip));
+                         final[pos_hex] = (char *)ntohs(tmp_ip);
+                        pos_hex++;
+                    } 
+            }
+            tmp_ip = atoi(num); 
+             final[pos_hex] = (char *)ntohs(tmp_ip);
+             pos_hex++;
+
+
+            // printf("%X\n", htons(port_recv));
+             final[pos_hex] = (char *)ntohs(port_recv);
+            // printf("%s\n", ip);
+            
+            printf("%s\n",final );
             break;
+            if (send(mysocket, final, strlen(final), 0) == -1){
+                perror("send");
+                exit(1);
+            }else{ 
+                printf("msg sent\n"); 
+            }
+            if ((numbytes = recv(mysocket, resposta, MAXRCVLEN, 0)) == -1){
+                perror("recv()");
+                exit(1);
+            }
+
+            resposta[numbytes] = '\0';
+            printf("%s\n", resposta );
+            // break;
 
         }
-        else if (resposta[0] == '0'){
+
+        if (resposta[0] == '0'){
 
             printf("mensagem resultado esta correta\n");       
             break;
@@ -112,9 +164,11 @@ int main(int argc, char *argv[])
             break;
 
         } 
+        else{
         printf("%i - Conecta a %s:%i\n",j,ip,port_recv );
         strcpy(resposta,conecta_ip_recv(ip,port_recv,msg));
         j++;
+        }
     }
    
     close(mysocket);
